0a4e6399799d3e092cc74f7c0d5e8015b7761001
Joshua Palmer <joshuavaughanpalmer@gmail.com>
2025-02-01 11:10:08 +0000

    Refactor to utilise more inline and remove multiple if statements

--- system-info/system-info.sh
+++ system-info/system-info.sh
@@ -15,61 +15,32 @@ echo "Hostname: $(hostname)"
 echo "Current User: $(whoami)"
 
 # Uptime Handling
-if [[ "$OS" == "macOS" ]]; then
-    echo "Uptime: $(uptime | awk -F'up ' '{print $2}' | awk -F',' '{print $1}')"
-else
-    echo "Uptime: $(uptime -p)"
-fi
+echo "Uptime: $(uptime -p 2>/dev/null || uptime | awk -F'up ' '{print $2}' | awk -F',' '{print $1}')"
 
 # Operating System
-if [[ "$OS" == "macOS" ]]; then
-    echo "Operating System: $(sw_vers -productName) $(sw_vers -productVersion)"
+if [[ "$OS" == "Darwin" ]]; then
+    OS_NAME="$(sw_vers -productName) $(sw_vers -productVersion)"
 else
-    echo "Operating System: $(uname -s) $(uname -r)"
+    OS_NAME="$(uname -s) $(uname -r)"
 fi
 
+echo "Operating System: $OS_NAME"
+
+
 # Memory Usage
-if [[ "$OS" == "macOS" ]]; then
-    # Get total memory in GB
-    MEM_TOTAL=$(sysctl -n hw.memsize)
-    MEM_TOTAL_GB=$((MEM_TOTAL / 1024 / 1024 / 1024))
-
-    # Get used memory from vm_stat
-    MEM_USED_PAGES=$(vm_stat | awk '/Pages active/ {print $3}' | sed 's/\.//')
-    MEM_USED_BYTES=$((MEM_USED_PAGES * 4096))
-    MEM_USED_GB=$((MEM_USED_BYTES / 1024 / 1024 / 1024))
-
+if [[ "$OS" == "Darwin" ]]; then
+    MEM_TOTAL_GB=$(( $(sysctl -n hw.memsize) / 1024 / 1024 / 1024 ))
+    MEM_USED_GB=$(( $(vm_stat | awk '/Pages active/ {print $3}' | sed 's/\.//') * 4096 / 1024 / 1024 / 1024 ))
     echo "Memory Usage: ${MEM_USED_GB}GB / ${MEM_TOTAL_GB}GB"
 else
-    # Linux: Use free -h wth formatting
     echo "Memory Usage: $(free -h | awk 'NR==2 {print $3 "/" $2}')"
 fi
 
 # Disk Usage
-if [[ "$OS" == "macOS" ]]; then
-    DISK_TOTAL=$(df -H / | awk 'NR==2 {print $2}') # Total space
-    DISK_USED=$(df -H / | awk 'NR==2 {print $3}') # Used space
-else
-    DISK_TOTAL=$(df -h --si / | awk 'NR==2') # Linux alternative
-    DISK_USED=$(df -h --si / | awk 'NR==2') # Linux alternative
-fi
-
-echo "Disk Usage: ${DISK_USED} used / ${DISK_TOTAL} total"
+echo "Disk Usage: $(df -H / | awk 'NR==2 {print $3 " used / " $2 " total"}' 2>/dev/null || df -h --si / | awk 'NR==2 {print $3 " used / " $2 " total"}')"
 
 # No. of active processes running
-if [[ "$OS" == "macOS" ]]; then
-    PROCESS_COUNT=$(ps -e | wc -l)
-else
-    PROCESS_COUNT=$(ps aux --no-heading | wc -l)
-fi
-
-echo "Processes Running: $PROCESS_COUNT"
+echo "Processes Running: $(ps -e | wc -l 2>/dev/null || ps aux --no-heading | wc -l)"
 
 # Display system IP address
-if [[ "$OS" == "macOS" ]]; then
-    IP_ADDRESS=$(ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
-else
-    IP_ADDRESS=$(ip -4 addr show | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
-fi
-
-echo "IP Address: $IP_ADDRESS"
+echo "IP Address: $(ifconfig 2>/dev/null | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1 || ip -4 addr show | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | cut -d'/' -f1 | head -n 1)"
